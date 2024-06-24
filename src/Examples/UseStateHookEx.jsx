c955667eeb1833f5906829c3ee2a678b27ceac66
Bahaa sobh <bhaasobh47@gmail.com>
2024-06-24 09:41:03 +0300

    use button component

--- src/Examples/UseStateHookEx.jsx
+++ src/Examples/UseStateHookEx.jsx
@@ -1,13 +1,14 @@
 import {React,useState} from 'react'
+import Button from '../components/Button';
 
 export default function UseStateHookEx() {
     const [count,setCount] = useState(0);
   return (
     <div>
       <h1>UseStateHookEx</h1>
-      <button className='btn' onClick={()=>setCount(count + 1)}>+</button>
+      <Button text={"+"} onClick={()=>setCount(count + 1)}/>
       <span>your number is {count}</span>
-      <button className='btn' onClick={()=>setCount(count - 1)}>-</button>
+      <Button text={"-"} onClick={()=>setCount(count - 1)}/>
     </div>
   )
 }
