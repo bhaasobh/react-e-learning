ff6a39c6429b15fea64fa6ead115b82d55796714
Bahaa sobh <bhaasobh47@gmail.com>
2024-06-25 09:26:57 +0300

    card finish

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import Header from './components/Header';
 import headerImg from "./img/header-img.png";
 import Button from './components/Button';
 import Modal from './components/Modal';
+import Card from './components/Card';
 //icons
-import { AiFillAlert  } from "react-icons/ai";
+import { AiFillHtml5  } from "react-icons/ai";
 //examples  
 import UseStateHookEx from './Examples/UseStateHookEx';
 
@@ -26,10 +27,14 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <>
+     <div className="container container-lg">
+      {/* {Modal} */}
   {showCourseModal&&<Modal title={"Access denied"} text = {"please login in order to access this content"} cancelevent={handelCancelEvent}/>}
+     {/* {Header} */}
      <Header>
-      <Title text='Learn to code by watching others' classes={'header-title fs-xxl mb-3'}/>
+      <div className='header-content mb-2'>
+ <Title text='Learn to code by watching others' classes={'header-title fs-xxl mb-3'}/>
       <p class="header-text mb-3">
             See how experienced developers solve problems in real-time. Watching
             scripted tutorials is great, but understanding how developers think
@@ -41,9 +46,24 @@ function App() {
 
           </div>
           <img src={headerImg} alt="header-img" className="header-img"/>
+
+      </div>
+     
      </Header>
+     {/* Main */}
+     <main>
+      {/* Catagories */}
+      <section className='categories my-4'>
+        <Title text='Top categories' classes={"subtitle text-center"}/>
+        <div className='categories-container d-flex'>
+          <Card icon_1={<AiFillHtml5 size = {40}/>}></Card>
+        </div>
+      </section>
+     </main>
      {/* <UseStateHookEx/> */}
     </div>
+    </>
+   
   );
 }
 
